8189a5dbe639ecdd1e21c12505fd93f2bceb3620
Timmy Jose <zoltan.jose@gmail.com>
2023-12-18 05:23:56 +0100

    chore: fix missing function for android

--- cpp/react-native-demo-provider.h
+++ cpp/react-native-demo-provider.h
@@ -5,6 +5,7 @@ extern "C" double rust_add(double, double);
 
 namespace demoprovider {
   double multiply(double a, double b);
+  double add(double a, double b);
 }
 
 #endif /* DEMOPROVIDER_H */
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-demo-provider';
+import { multiply, add } from 'react-native-demo-provider';
 
 const result = multiply(3, 7);
+const sum = add(3, 7);
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Result: {result}</Text>
+      <Text>Sum: {sum}</Text>
     </View>
   );
 }
